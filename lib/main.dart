c015b4d8c5e84c588cea340c6413a09616dd4f94
Thomas Hayner <thom.hayner@gmail.com>
2021-07-03 20:54:31 -0600

    remove old comments

--- lib/main.dart
+++ lib/main.dart
@@ -20,18 +20,14 @@ class DicePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: Row(
-        // mainAxisAlignment has no effect on padding within row due to expand
-        // mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Expanded(
-            // flex: 2,
             child: Padding(
               padding: const EdgeInsets.all(16.0),
               child: Image.asset('images/dice1.png'),
             ),
           ),
           Expanded(
-            // flex: 5,
             child: Padding(
               padding: const EdgeInsets.all(16.0),
               child: Image.asset('images/dice2.png'),
